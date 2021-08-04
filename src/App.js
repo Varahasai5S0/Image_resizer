a008108f685bafdceb603bd7677acf83f7b3286a
varahasai <sachinrohit145@gmail.com>
2021-08-04 11:29:07 +0530

    added index file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Input from './Input'; 
 import './index.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 const App = () => {
   return (
